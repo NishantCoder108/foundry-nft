696c2fcd6e37fee6f7d5466cdc929dd799dcc266
NISHANT <69218488+NishantCoder108@users.noreply.github.com>
2024-12-29 19:26:52 +0000

    Write contract of mint nft

--- src/BasicNft.sol
+++ src/BasicNft.sol
@@ -7,11 +7,22 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 contract BasicNft is ERC721 {
     uint256 private s_tokenCounter;
 
+    mapping(uint256 => string) private s_tokenIdToUri; //mapping counterNumber with hashing string
     constructor() ERC721("Doggie", "DOG") {
-        s_tokenCounter = 0;
+        s_tokenCounter = 0; //for every new nft ,we will increment this counter
+    }
+
+    function mintNFT(string memory tokenUri) public returns (uint256) {
+        s_tokenIdToUri[s_tokenCounter] = tokenUri;
+
+        _safeMint(msg.sender, s_tokenCounter);
+        s_tokenCounter++;
     }
 
     function tokenURI(
+        //tokenuri is a function that returns the metadata of the nft
         uint256 _tokenId
-    ) public view override returns (string memory) {}
+    ) public view override returns (string memory) {
+        return s_tokenIdToUri[_tokenId];
+    }
 }
