9188e91d60243ba610421e327102542828434a0b
NISHANT <69218488+NishantCoder108@users.noreply.github.com>
2024-12-31 06:40:44 +0000

    Test name by keccak256

--- test/BasicNftTest.t.sol
+++ test/BasicNftTest.t.sol
@@ -17,10 +17,18 @@ contract BasicNftTest is Test {
         basicNft = deployer.run();
     }
 
-    function testNameIsCorrect() public {
+    function testNameIsCorrect() public view {
         string memory expectedName = "Doggie";
         string memory actualName = basicNft.name();
 
-        assertEq(actualName, expectedName);
+        // assertEq(actualName, expectedName);
+
+        //abi.encodePacked will convert to bytes
+        //keccak256 will hash the bytes into bytes32
+
+        assert(
+            keccak256(abi.encodePacked(actualName)) ==
+                keccak256(abi.encodePacked(expectedName))
+        );
     }
 }
