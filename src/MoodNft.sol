d26472787636119e89ef4189160faa58a6e5c47b
NISHANT <69218488+NishantCoder108@users.noreply.github.com>
2025-01-01 17:31:35 +0000

    Add FlipMood function , with condition getApproved and isOwner

--- src/MoodNft.sol
+++ src/MoodNft.sol
@@ -4,8 +4,12 @@ pragma solidity ^0.8.28;
 
 import {Base64} from "@openzeppelin/contracts/utils/Base64.sol";
 import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 contract MoodNft is ERC721 {
+    // error
+    error MoodNFT_CantFlipMoodIfNotOwner();
+    error MoodNFT_CallerIsNotOwnerNorApproved();
+
     string private sadSvg;
     string private happySvg;
     uint256 private s_tokenCounter;
@@ -18,7 +22,10 @@ contract MoodNft is ERC721 {
     mapping(uint256 => Mood) private s_tokenIdToMood;
 
     //here , svg is storing on onchain
-    constructor(string memory _sadSvg, string memory _happySvg) ERC721("MoodNft", "MOOD") {
+    constructor(
+        string memory _sadSvg,
+        string memory _happySvg
+    ) ERC721("MoodNft", "MOOD") {
         sadSvg = _sadSvg;
         happySvg = _happySvg;
 
@@ -32,11 +39,31 @@ contract MoodNft is ERC721 {
         s_tokenCounter++;
     }
 
+    function flipMood(uint256 tokenId) public {
+        // Check if the token exists
+        address owner = ownerOf(tokenId);
+        require(owner != address(0), "Token does not exist");
+
+        // Check if the caller is the owner or approved
+        require(
+            msg.sender == owner || msg.sender == getApproved(tokenId),
+            MoodNFT_CallerIsNotOwnerNorApproved()
+        );
+
+        if (s_tokenIdToMood[tokenId] == Mood.SAD) {
+            s_tokenIdToMood[tokenId] = Mood.HAPPY;
+        } else {
+            s_tokenIdToMood[tokenId] = Mood.SAD;
+        }
+    }
+
     function _baseURI() internal pure override returns (string memory) {
         return "data:application/json;base64,";
     }
 
-    function tokenURI(uint256 tokenId) public view override returns (string memory) {
+    function tokenURI(
+        uint256 tokenId
+    ) public view override returns (string memory) {
         string memory imageURI;
         if (s_tokenIdToMood[tokenId] == Mood.HAPPY) {
             imageURI = happySvg;
@@ -45,21 +72,22 @@ contract MoodNft is ERC721 {
         }
 
         //concating the metadata of the nft
-        return string(
-            abi.encodePacked(
-                _baseURI(),
-                Base64.encode( //convert to base64
-                    bytes(
-                        abi.encodePacked( //convert to bytes
-                            '{"name: "',
-                            name(),
-                            '", description: "An NFT that reflects your mood!", "attributes": [{"trait_type": "Mood", "value": 100}], "image": ',
-                            imageURI,
-                            '"}'
+        return
+            string(
+                abi.encodePacked(
+                    _baseURI(),
+                    Base64.encode( //convert to base64
+                            bytes(
+                                abi.encodePacked( //convert to bytes
+                                        '{"name: "',
+                                        name(),
+                                        '", description: "An NFT that reflects your mood!", "attributes": [{"trait_type": "Mood", "value": 100}], "image": ',
+                                        imageURI,
+                                        '"}'
+                                    )
+                            )
                         )
-                    )
                 )
-            )
-        );
+            );
     }
 }
